3c571ca6cdb5d755b3d324659b71c001b38345e7
Yusuf Tekin <yusuf.tekin@kofteciyusuf.com.tr>
2021-10-18 19:10:05 +0300

    Initial commit

---
+++ app/src/main/java/com/ace1ofspades/gittest2/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ace1ofspades.gittest2.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ace1ofspades.gittest2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
